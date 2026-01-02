c9f72a5f8c55d2e5d60fb09341713d06edec1a22
KamilKonradCiesielski <138165824+KamilKonradCiesielski@users.noreply.github.com>
2026-01-02 13:08:40 +0100

    Update main.c

--- 03_Arduino_Piano/main.c
+++ 03_Arduino_Piano/main.c
@@ -1,1 +1,32 @@
+void setup() {
+ pinMode(7, INPUT_PULLUP);
+ pinMode(6, INPUT_PULLUP);
+ pinMode(5, INPUT_PULLUP);
+ pinMode(4, INPUT_PULLUP);
 
+ digitalWrite(3, OUTPUT); 
+}
+
+void loop() {
+
+if (digitalRead(7) == LOW) {
+	analogWrite(3, 255);	
+	delay(1000);
+	analogWrite(3,0);
+}
+if (digitalRead(6) == LOW) {
+	analogWrite(3,191);
+	delay(1000);
+	analogWrite(3,0);
+}
+if (digitalRead(5) == LOW) {
+	analogWrite(3,127);
+	delay(1000);
+	analogWrite(3,0);
+}
+if (digitalRead(4) == LOW){
+	analogWrite(3,63);
+	delay(1000);
+	analogWrite(3,0);
+}
+}
